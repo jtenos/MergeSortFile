b4bdfb28fc735b12e936e7139f8b179e6b060908
Joe Enos <jtenos1@gmail.com>
2022-04-06 16:04:52 -0700

    Moving to separate directory and adding console tester application.

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,40 @@
+﻿using MergeSortFile;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<LineSorter>();
+        services.AddHostedService<Worker>();  
+    })
+    .Build()
+    .Run();
+
+class Worker
+    : BackgroundService
+{
+    private readonly LineSorter _lineSorter;
+
+    public Worker(LineSorter lineSorter)
+    {
+        _lineSorter = lineSorter;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using (StreamWriter writer = new(@"C:\temp\file1.txt"))
+        {
+            for (int i = 0; i < 5_000_000; ++i)
+            {
+                if (i % 100_000 == 0) { Console.WriteLine($"{i:#,##0}"); }
+                writer.WriteLine(Guid.NewGuid().ToString("N"));
+            }
+        }
+        _lineSorter.SortFile(@"C:\temp\file1.txt", @"C:\temp\file1.txt.sorted", numLinesPerTempFile: 10_000);
+
+        Console.WriteLine("DONE");
+        Environment.Exit(0);
+        return Task.CompletedTask;
+    }
+}
